a0bc1c3b88b7369fb54b29d51dc74de1f17c8b26
uhling19 <Uhling19@up.edu>
2017-02-16 20:31:26 -0800

    change

--- app/src/main/java/edu/up/cs371/textmod/TextModActivity.java
+++ app/src/main/java/edu/up/cs371/textmod/TextModActivity.java
@@ -5,9 +5,11 @@ package edu.up.cs371.textmod;
  *
  * Allow text to be modified in simple ways with button-presses.
  */
+import android.app.Activity;
 import android.content.res.TypedArray;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -16,8 +18,16 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemSelectedListener;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Spinner;
+import android.widget.TextView;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
+
 import java.util.ArrayList;
 
 public class TextModActivity extends ActionBarActivity {
@@ -29,22 +39,36 @@ public class TextModActivity extends ActionBarActivity {
     private ImageView imageView; // the view that shows the image
 
     /**
-     * @see android.app.Activity#onCreate(android.os.Bundle)
+     * @see Activity#onCreate(Bundle)
      */
-    @Override
+
+    private Button button;
+    private TextView text;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
+
     protected void onCreate(Bundle savedInstanceState) {
+
 
         // perform superclass initialization; load the layout
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_text_mod);
 
         // set instance variables for our widgets
-        imageView = (ImageView)findViewById(R.id.imageView);
+        imageView = (ImageView) findViewById(R.id.imageView);
+
+        button = (Button) findViewById(R.id.button);
+        text = (TextView) findViewById(R.id.editText);
+        button.setOnClickListener(new cButtonListener());
+
 
         // Set up the spinner so that it shows the names in the spinner array resources
         //
         // get spinner object
-        Spinner spinner = (Spinner)findViewById(R.id.spinner);
+        Spinner spinner = (Spinner) findViewById(R.id.spinner);
         // get array of strings
         String[] spinnerNames = getResources().getStringArray(R.array.spinner_names);
         // create adapter with the strings
@@ -63,20 +87,74 @@ public class TextModActivity extends ActionBarActivity {
         // loop through, adding one image per string
         for (int i = 0; i < spinnerNames.length; i++) {
             // determine the index; use 0 if out of bounds
-            int id = imageIds2.getResourceId(i,0);
-            if (id == 0) id = imageIds2.getResourceId(0,0);
+            int id = imageIds2.getResourceId(i, 0);
+            if (id == 0) id = imageIds2.getResourceId(0, 0);
             // load the image; add to arraylist
             Bitmap img = BitmapFactory.decodeResource(getResources(), id);
             images.add(img);
         }
 
+
         // define a listener for the spinner
         spinner.setOnItemSelectedListener(new MySpinnerListener());
 
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "TextMod Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app URL is correct.
+                Uri.parse("android-app://edu.up.cs371.textmod/http/host/path")
+        );
+        AppIndex.AppIndexApi.start(client, viewAction);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "TextMod Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app URL is correct.
+                Uri.parse("android-app://edu.up.cs371.textmod/http/host/path")
+        );
+        AppIndex.AppIndexApi.end(client, viewAction);
+        client.disconnect();
+    }
+
+
+    private class cButtonListener implements View.OnClickListener {
+
+        @Override
+        public void onClick(View v) {
+            text.setText(" ");
+        }
     }
 
     /**
-     * @see android.app.Activity#onCreateOptionsMenu(android.view.Menu)
+     * @see Activity#onCreateOptionsMenu(Menu)
      */
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -86,7 +164,7 @@ public class TextModActivity extends ActionBarActivity {
     }
 
     /**
-     * @see android.app.Activity#onOptionsItemSelected(android.view.MenuItem)
+     * @see Activity#onOptionsItemSelected(MenuItem)
      */
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
@@ -109,8 +187,8 @@ public class TextModActivity extends ActionBarActivity {
     private class MySpinnerListener implements OnItemSelectedListener {
 
         /**
-         * @see android.widget.AdapterView.OnItemSelectedListener#onItemSelected(
-         *                  android.widget.AdapterView, android.view.View, int, long)
+         * @see OnItemSelectedListener#onItemSelected(
+         *AdapterView, View, int, long)
          */
         @Override
         public void onItemSelected(AdapterView<?> parentView, View selectedItemView,
@@ -120,8 +198,8 @@ public class TextModActivity extends ActionBarActivity {
         }
 
         /**
-         * @see android.widget.AdapterView.OnItemSelectedListener#onNothingSelected(
-         *                  android.widget.AdapterView)
+         * @see OnItemSelectedListener#onNothingSelected(
+         *AdapterView)
          */
         @Override
         public void onNothingSelected(AdapterView<?> parentView) {
